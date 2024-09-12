6fc239129dfa12ffd8ebc057e4685a66e724d234
magaldevs <danielgalfla12@gmail.com>
2024-09-12 11:38:45 -0300

    União de Vetores

---
+++ src/coisaFragoso/Uniao_Vetores.java
@@ -0,0 +1,44 @@
+package coisaFragoso;
+
+import java.util.Scanner;
+
+public class Uniao_Vetores {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        Integer[] A = new Integer[5]; 
+        int C = 0; 
+        
+        while(C < 5) {
+            if(C == 0) {
+                System.out.print("Digite um valor: ");
+                A[C] = scanner.nextInt();
+                C++;  
+            } else {
+                int chave = 0;
+                System.out.print("Digite um valor: ");
+                int valor = scanner.nextInt();
+                
+                for(int i = 0; i < C; i++) {
+                    if(A[i] == valor) {
+                        chave = 1;
+                        break;
+                    }
+                }
+                
+               
+                if(chave == 0) {
+                    A[C] = valor;
+                    C++;
+                } else {
+                    System.out.println("Valor já existe. Tente outro valor.");
+                }
+            }
+        }
+        
+        System.out.println("Valores armazenados:");
+        for(int i = 0; i < A.length; i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
